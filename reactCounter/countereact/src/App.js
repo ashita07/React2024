721494d4c39f537a41520343fa179f1da8c49556
Ashita <ashitam2407@gmail.com>
2024-09-02 01:32:11 +0530

    bettered the counter

--- reactCounter/countereact/src/App.js
+++ reactCounter/countereact/src/App.js
@@ -6,14 +6,12 @@ function App() {
   return (
     <div>
       <Step step={step} setstep={setStep} />
-      <Counter count={step} />
+      <Counter count={step} setStep={setStep} />
     </div>
   );
 }
 
 function Step({ step, setstep }) {
-  let Inc = () => setstep(step + 1);
-  let Dec = () => setstep(step - 1);
   return (
     <div
       style={{
@@ -23,18 +21,28 @@ function Step({ step, setstep }) {
         alignItems: "center",
       }}
     >
+      <input
+        type="range"
+        min="0"
+        max="10"
+        value={step}
+        onChange={(e) => setstep(Number(e.target.value))}
+      />{" "}
       Step{step}
-      <div>
-        <button onClick={Dec}>-</button>
-        <button onClick={Inc}>+</button>
-      </div>
+      <div></div>
     </div>
   );
 }
-function Counter({ count }) {
+
+function Counter({ count, setStep }) {
   const [counter, setCounter] = useState(0);
   let IncreaseCount = () => setCounter(counter + count);
   let DeacreaseCount = () => setCounter(counter - count);
+
+  function HandleReset() {
+    setCounter(1);
+    setStep(0);
+  }
 
   const date = new Date("August 11 2024");
   date.setDate(date.getDate() + counter);
@@ -48,21 +56,30 @@ function Counter({ count }) {
         alignItems: "center",
       }}
     >
-      Counter {counter}
-      <div>
+      <span>
         <button onClick={DeacreaseCount}>-</button>
+        <input
+          type="text"
+          value={counter}
+          onChange={(e) => setCounter(Number(e.target.value))}
+        />
         <button onClick={IncreaseCount}>+</button>
-      </div>
+      </span>
       <p>
         <span>
           {count === 0
             ? "today is "
             : counter > 0
-            ? `${counter} days from today is`
-            : `${Math.abs(counter)} days ago was`}
+            ? `${counter} days from today is `
+            : `${Math.abs(counter)} days ago was `}
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      <div>
+        {counter > 1 || count > 0 ? (
+          <button onClick={HandleReset}>Reset</button>
+        ) : null}
+      </div>
     </div>
   );
 }
